80c0bcec55d5f782d9aa0410e832701b29d260ab
Sofie Vinjerui Berge <sofievb@student.uia.no>
2024-04-08 13:26:17 +0200

    Updated to a second matching iteration and corrected one hot encoding function

--- data_processing/process_utstyr.py
+++ data_processing/process_utstyr.py
@@ -1,41 +1,32 @@
-from cmath import inf
-from os import read, replace
-from re import sub
 from unittest import result
 import pandas as pd
-import json
 import ast
-import numpy as np
-import sys
-from polyfuzz.models import EditDistance, TFIDF, Embeddings, RapidFuzz
+from polyfuzz.models import EditDistance
 from polyfuzz import PolyFuzz
-from collections import defaultdict
-from sympy import li
 from tqdm import tqdm
+from itertools import islice
 def read_data(filepath):
     df_utstyr = pd.read_csv(filepath)
     #Convert list of equipment from dtype str to list(Finnkode, "[""x"",""y""]" => Finnkode,[x,y])
     
     df_utstyr['Equipment'] = df_utstyr['Equipment'].apply(ast.literal_eval)
-    #Count occurences of equipments
-    all_equipment = df_utstyr['Equipment'].explode().value_counts()
-    
-    return df_utstyr, all_equipment
 
-def standarize_equipment(df):
     
+    return df_utstyr
+def standarize_equipment(df):
     for idx, row in df.iterrows():
-        cleaned_astr = [item.replace('*','')for item in row['Equipment']if isinstance(item,str)]
-        cleaned_quest = [item.replace('?','') for item in cleaned_astr if isinstance(item, str)]
-        lowercase = [item.lower()  for item in cleaned_quest]
-        df.at[idx, 'Equipment'] = lowercase
+            cleaned_astr = [item.replace('?', '')for item in row['Equipment']if isinstance(item,str)]
+            lowercase = [item.lower()  for item in cleaned_astr]
+            df.at[idx, 'Equipment'] = lowercase
         
-    return df
+            #Count occurences of equipments
+    all_equipment = df['Equipment'].explode().value_counts()
+    return df, all_equipment
 
 def low_freq_equipment(equipment_list):
        
     #Find equipments occuring once
-    mask_occurs_once = equipment_list <= 1
+    mask_occurs_once = equipment_list == 1
     items_occurs_once = equipment_list[mask_occurs_once]
     #convert equipment of once/twice occurrence to list and retrieve string(index)
     list_low_freq_equipment = items_occurs_once.index.to_list()
@@ -62,17 +53,19 @@ def matchesfound(clusters,all_standardized_equipment, min_similarity):
 
     #top equipment list
     top_equipment = all_standardized_equipment.head(50).index.to_list()
-    model = PolyFuzz("TF-IDF")
+    tqdm.write("Initializing PolyFuzz model for matches...")
 
+    model = PolyFuzz("TF-IDF")
+    tqdm.write("Matching with top equipment")
     model.match(from_list_flat, top_equipment)
+    tqdm.write("Matching with top equipment")
     matches =  model.get_matches()
     best_match = matches[matches['Similarity']>=min_similarity]
-    
     
     return matches, best_match
 
 
-def apply_standardization(df, best_match, new_filename):
+def apply_standardization(df, best_match):
     # Create a dictionary mapping from original to standardized names
     # Assuming 'From' is the column with original names and 'To' is the column with standardized names
     standardization_map = pd.Series(best_match['To'].values, index=best_match['From']).to_dict()
@@ -80,12 +73,14 @@ def apply_standardization(df, best_match, new_filename):
     # Apply the mapping to each equipment name in the 'Equipment' column
     def standardize_equipment(equipment_list):
         if isinstance(equipment_list, str):
-            equipment_list = ast.literal_eval(equipment_list)        # Replace each item in the list with its standardized form, if available
+            equipment_list = ast.literal_eval(equipment_list)
+            print(equipment_list)
+                    # Replace each item in the list with its standardized form, if available
         return [standardization_map.get(item, item) for item in equipment_list]
 
     df['Equipment'] = df['Equipment'].apply(standardize_equipment)
-    new_csv = df.to_csv(new_filename, index=False)
-    return df, standardization_map, new_csv
+
+    return df, standardization_map
 
 def find_matches_with_top_equipment(low_freq_list, top_equipment_list, min_similarity=0.65):
     # Initialize PolyFuzz model for individual string matching
@@ -105,80 +100,60 @@ def find_matches_with_top_equipment(low_freq_list, top_equipment_list, min_simil
 
     return all_matches
 
-if __name__ == "__main__":
-    '''
-    filepathv0 = 'full_dataset/all_equipments.csv'
-    df_utstyr_v0, all_equipment = read_data(filepath=filepathv0)
-    df = standarize_equipment(df_utstyr_v0)
-    all_standardized_equipment = df['Equipment'].explode().value_counts()
-
-    equip_list = all_standardized_equipment.index.to_list()    
-    top_equipment = all_standardized_equipment.head(50).index.to_list()
-
-    #print(equip_list)
-    one_occurence_equipment = low_freq_equipment(all_standardized_equipment)
-    print(f"Number of equipments occuring once: {len(one_occurence_equipment)}")
-    print('Total number of equipments:',len(all_standardized_equipment.index))
-    
-    #cluster matches:
-    result = polyfuzz_grouping(equip_list)
-    print(result)
-    #find matches with existing equipment
-    m, best_match = matchesfound(result,all_standardized_equipment, min_similarity=0.8)
-    print('Match', m,'\n', best_match)
-    new_df, standardization_map, new_csv = apply_standardization(df, best_match, new_filename='data/equipment_1st_match.csv')
-    #create new csv based on matching
-    '''
-    
-    filepathv1 = 'data/equipment_1st_match.csv'
-    df_utstyr_v1, all_equipments = read_data(filepath=filepathv1)
-    df_utstyr = standarize_equipment(df_utstyr_v1)
-    all_standard_equipment = df_utstyr['Equipment'].explode().value_counts()
-
-    equipment_list = all_standard_equipment.index.to_list()
-    once_occur_euqipment = low_freq_equipment(all_standard_equipment)
-    print(f"Number of equipments occuring once: {len(once_occur_euqipment)}")
-    print('Total number of equipments:',len(all_standard_equipment.index))
-    print(f"Top 50 equipment: {all_standard_equipment.head(37)}")
-    '''
-    result = polyfuzz_grouping(equipment_list)
-    print(result)
-    #find matches with existing equipment
-    m, best_match = matchesfound(result,all_standard_equipment, min_similarity=0.8)
-    print('Match', m,'\n', best_match)
-    new_df, standardization_map, new_csv = apply_standardization(df_utstyr_v1, best_match, new_filename='data/equipment_2nd_match.csv')
-    print(all_standard_equipment.head(37))
-    '''
-    '''
-    filepath_v2 = 'data/equipment_2nd_match.csv'
-    df_utstyr_v2, all_equipments_v2 = read_data(filepath=filepath_v2)
-    df_utstyrv2 = standarize_equipment(df_utstyr_v2)
-    all_standard_equipment_v2 = df_utstyrv2['Equipment'].explode().value_counts()
-
-    equipment_list_v2 = all_standard_equipment_v2.index.to_list()
-    once_occur_euqipment_v2 = low_freq_equipment(all_standard_equipment_v2)
-    print(f"Number of equipments occuring once: {len(once_occur_euqipment_v2)}")
-    print('Total number of equipments:',len(all_standard_equipment_v2.index))
-    print(f"Top 50 equipment: {all_standard_equipment_v2.head(37)}")
-    #print('Bt matches', matchi)
-
-    result = polyfuzz_grouping(equipment_list_v2)
-    print(result)
-    #find matches with existing equipment
-    matches_v2, best_match_v2 = matchesfound(result,all_standard_equipment_v2, min_similarity=0.75)
-    print('Match', matches_v2,'\n', best_match_v2[best_match_v2['Similarity']<=0.75])
-    '''
-    #creating a csv of top 37 equipment(the same no columns as in specs)
+def one_hot_encode_equipment(toplist, df_utstyr):
     #creating a column of each equipment
-    toplist = all_standard_equipment.head(37).index.to_list()
     for equipment in toplist:
         df_utstyr[equipment] = 0
 
-    for index, row in df_utstyr.iterrows():
-        equipments_in_row = row['Equipment']
 
-        for equipment in equipments_in_row:
-            if equipment in toplist:
-                df_utstyr.at[index, equipment] = 1
     df_utstyr_dropped = df_utstyr.drop('Equipment', axis=1)
-    df_utstyr_dropped.to_csv('data/equipment_1st_match_one_hot_encoded.csv', index=False)
+    df_utstyr_dropped.to_csv('data/equipment_2nd_match_one_hot_encoded.csv', index=False)
+    
+if __name__ == "__main__":
+    '''
+    #Reading 1st polyfuzz prosessed data
+    filepathv0 = 'data/equipments_1st_match.csv'
+    df_utstyr = read_data(filepath=filepathv0)
+
+    #remove special chars and return full equipment list
+    df_utstyr_v0,all_equipment = standarize_equipment(df_utstyr)
+    #list of equipment names
+    equipment_list = all_equipment.index.to_list()
+
+    top_equipment = all_equipment.head(n=37)  
+    one_occurence_equipment = low_freq_equipment(all_equipment)
+    print(top_equipment)
+    
+    print(f"Number of equipments occuring once: {len(one_occurence_equipment)}")
+    print('Total number of equipments:',len(all_equipment.index))
+    print(f"Top 37 equipment: \n {top_equipment}")
+    #polyfuzz model for match clusters:
+    result = polyfuzz_grouping(equipment_list)
+
+    #find matches with top equipments, and matches above min_similarity
+    match, best_match = matchesfound(result,all_equipment, min_similarity=0.73)
+    print(best_match)
+    new_df, standardization_map = apply_standardization(df=df_utstyr_v0, best_match=best_match)
+    new_df.to_csv('data/equipments_2nd_match.csv', index=False)
+    print(new_df)
+    print('\n Standards: \n')
+    print(standardization_map)
+    one_hot_encode_equipment(toplist=top_equipment, df_utstyr=new_df)
+    
+    '''
+    #Second polyfuzz processed data?
+    filepath_v2 = 'data/equipments_2nd_match.csv'
+    df_utstyr2 = read_data(filepath_v2)
+
+
+    df_utstyr_v2,all_equipment = standarize_equipment(df_utstyr2)
+    #list of equipment names
+    equipment_list = all_equipment.index.to_list()
+    top_equipment = all_equipment.head(n=37)
+
+    one_occurence_equipment = low_freq_equipment(all_equipment)
+    print(f"Number of equipments occuring once: {len(one_occurence_equipment)}")
+    print('Total number of equipments:',len(all_equipment.index))
+    print(f"Top 37 equipment: \n {top_equipment}")
+
+    one_hot_encode_equipment(toplist=top_equipment.index.to_list(), df_utstyr=df_utstyr_v2)
